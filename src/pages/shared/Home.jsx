a2bd1e10572135077e842d858183b34a23e47b98
finnlamond <128053954+finnlamond@users.noreply.github.com>
2024-03-05 22:27:29 +0000

    Initial commit

---
+++ src/pages/shared/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <>
+            <h1 className="text=3x1 font-bold-underline">Home</h1>
+        </>
+    )
+}
+export default Home;
